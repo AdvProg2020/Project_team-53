9fd646b0e3b0b9803e686ed8e2f33f5c696aab9f
Parham <parhampch@gmail.com>
2020-05-12 01:40:41 +0430

    complete ProductsMenu

--- src/main/java/View/Menu/ProductsMenus/ProductsMenu.java
+++ src/main/java/View/Menu/ProductsMenus/ProductsMenu.java
@@ -1,5 +1,6 @@
 package View.Menu.ProductsMenus;
 
+import View.Menu.AdminMenus.ManageAllProductsMenu;
 import View.Menu.Menu;
 
 import java.util.regex.Matcher;
@@ -10,6 +11,8 @@ public class ProductsMenu extends Menu {
         super.addToSubMenus(1, this.getViewAllCategoriesMenu());
         super.addToSubMenus(2, new FilteringMenu(this));
         super.addToSubMenus(3, new SortingMenu(this));
+        super.addToSubMenus(4, this.getShowProductsMenu());
+        super.addToSubMenus(5, this.getProductMenu());
     }
 
     private Menu getViewAllCategoriesMenu()
@@ -34,6 +37,73 @@ public class ProductsMenu extends Menu {
                     }
                     else
                         throw new Exception("Invalid Input");
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                }
+                this.execute();
+            }
+        };
+    }
+
+    private Menu getShowProductsMenu()
+    {
+        return new Menu("Show Products Menu", this) {
+            @Override
+            public void show() {
+                System.out.println("All products are:\n(Enter back to return");
+            }
+
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                try
+                {
+                    Matcher matcher2 = getMatcher(input, "^\\s*back\\s*$");
+                    if(matcher2.find())
+                    {
+                        this.parentMenu.show();
+                        this.parentMenu.execute();
+                        return;
+                    }
+                    else
+                        throw new Exception("Invalid Input");
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                }
+                this.execute();
+            }
+        };
+    }
+
+    private Menu getProductMenu()
+    {
+        return new Menu("Product Menu", this) {
+            @Override
+            public void show() {
+                System.out.println("Please enter productID\n(Enter back to return");
+            }
+
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                try
+                {
+                    Matcher matcher1 = getMatcher(input, "");
+                    Matcher matcher2 = getMatcher(input, "^\\s*back\\s*$");
+                    if(matcher2.find())
+                    {
+                        this.parentMenu.show();
+                        this.parentMenu.execute();
+                        return;
+                    }
+                    else if(!matcher1.find())
+                    {
+                        throw new Exception("invalid input");
+                    }
                 }
                 catch (Exception e)
                 {
