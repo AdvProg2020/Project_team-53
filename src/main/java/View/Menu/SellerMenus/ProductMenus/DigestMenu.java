654b803014245cea1c2ac81f9033c50cc4e8d6b4
Parham <parhampch@gmail.com>
2020-05-12 19:05:48 +0430

    add ProductMenu and DigestMenu

---
+++ src/main/java/View/Menu/SellerMenus/ProductMenus/DigestMenu.java
@@ -0,0 +1,81 @@
+package View.Menu.SellerMenus.ProductMenus;
+
+import View.Menu.Menu;
+
+import java.util.regex.Matcher;
+
+public class DigestMenu extends Menu {
+
+    public DigestMenu(Menu parentMenu) {
+        super("Digest Menu", parentMenu);
+        super.addToSubMenus(1, this.getProductInformationMenu());
+        super.addToSubMenus(2, this.getAddToCartMenu());
+    }
+
+    private Menu getProductInformationMenu()
+    {
+        return new Menu("Product Information Menu", this) {
+            @Override
+            public void show() {
+                System.out.println("Product information is:\n(Enter back to return)");
+            }
+
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                try
+                {
+                    Matcher matcher2 = getMatcher(input, "^\\s*back\\s*$");
+                    if(matcher2.find())
+                    {
+                        this.parentMenu.show();
+                        this.parentMenu.execute();
+                        return;
+                    }
+                    else
+                        throw new Exception("Invalid Input");
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                }
+                this.execute();
+            }
+        };
+    }
+
+    private Menu getAddToCartMenu()
+    {
+        return new Menu("Add To Cart Menu", this) {
+            @Override
+            public void show() {
+                System.out.println("Please enter seller:\n(Enter back to return");
+            }
+
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                try
+                {
+                    Matcher matcher1 = getMatcher(input, "");
+                    Matcher matcher2 = getMatcher(input, "^\\s*back\\s*$");
+                    if(matcher2.find())
+                    {
+                        this.parentMenu.show();
+                        this.parentMenu.execute();
+                        return;
+                    }
+                    else if(!matcher1.find())
+                    {
+                        throw new Exception("invalid input");
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                }
+                this.execute();
+            }
+        };
+    }
+}
---
+++ src/main/java/View/Menu/SellerMenus/ProductMenus/ProductMenu.java
@@ -0,0 +1,11 @@
+package View.Menu.SellerMenus.ProductMenus;
+
+import View.Menu.Menu;
+
+public class ProductMenu extends Menu {
+    public ProductMenu(Menu parentMenu) {
+        super("Product Menu", parentMenu);
+    }
+
+
+}
