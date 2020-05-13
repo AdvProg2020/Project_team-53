a766339795a417a1083c788a7ac33bdcf0aee105
Parham <parhampch@gmail.com>
2020-05-14 03:12:05 +0430

    make change in ViewOrdersMenu

--- src/main/java/View/Menu/BuyerMenus/ViewOrdersMenu.java
+++ src/main/java/View/Menu/BuyerMenus/ViewOrdersMenu.java
@@ -2,11 +2,13 @@ package View.Menu.BuyerMenus;
 
 import View.Menu.Menu;
 
+import java.util.regex.Matcher;
+
 public class ViewOrdersMenu extends Menu {
     public ViewOrdersMenu(Menu parentMenu) {
         super("View Orders Menu", parentMenu);
         super.addToSubMenus(1, this.getShowOrderMenu());
-        super.addToSubMenus(2, this.getRateMenu());
+        super.addToSubMenus(2, this.getGiveRateMenu());
     }
 
     private Menu getShowOrderMenu()
@@ -27,9 +29,9 @@ public class ViewOrdersMenu extends Menu {
         };
     }
 
-    private Menu getRateMenu()
+    private Menu getGiveRateMenu()
     {
-        return new Menu("Rate Menu", this) {
+        return new Menu("Give Rate Menu", this) {
             @Override
             public void show() {
                 System.out.println("Please enter your productID and your rate 1 to 5:\n(Enter back to return");
@@ -38,7 +40,27 @@ public class ViewOrdersMenu extends Menu {
             @Override
             public void execute() {
                 String input = scanner.nextLine();
-                //todo
+                try
+                {
+                    Matcher matcher1 = getMatcher(input, "^\\s*(\\d+)\\s+([1-5])\\s*$");
+                    Matcher matcher2 = getMatcher(input, "^\\s*back\\s*$");
+                    if(matcher2.find())
+                    {
+                        this.parentMenu.show();
+                        this.parentMenu.execute();
+                        return;
+                    }
+                    else if(!matcher1.find())
+                    {
+                        throw new Exception("invalid input");
+                    }
+                    else {
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                }
                 this.execute();
             }
         };
