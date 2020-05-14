83dca9dea11fba36dc8b855c708710b2a13797e8
Informatic <msaadat1379@gmail.com>
2020-05-15 00:16:28 +0430

    Sort added

--- src/main/java/Controller/AllProductManager.java
+++ src/main/java/Controller/AllProductManager.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 public class AllProductManager {
 
+    static ArrayList<Product> allProducts = Database.getAllProducts();
+
     public static String goToProduct(int productId){
         if (Database.getProductByID(productId) == null){
             return "No product with this Id";
@@ -20,11 +22,46 @@ public class AllProductManager {
 
     public static String showAllProduct(){
         StringBuilder ans = new StringBuilder();
-        ArrayList<Product> allProducts = Database.getAllProducts();
         for (Product product : allProducts) {
             ans.append(product.digest());
             ans.append("\n----------------------\n");
         }
         return ans.toString();
+    }
+
+    public static String ignoreAllFilterAndSort(){
+        allProducts = Database.getAllProducts();
+        return  showAllProduct();
+    }
+
+    public static String sortByName(){
+        allProducts.sort(AllProductManager::compareWithName);
+        return showAllProduct();
+    }
+
+    public static String sortByPrice(){
+        allProducts.sort(AllProductManager::compareWithPrice);
+        return showAllProduct();
+    }
+
+    public static String sortByScore(){
+        allProducts.sort(AllProductManager::compareWithScore);
+        return showAllProduct();
+    }
+
+    public static int compareWithPrice(Product p1, Product p2){
+        return p1.getPrice() > p2.getPrice() ? 1 : -1;
+    }
+
+    public static int compareWithName(Product p1, Product p2){
+        return p1.getName().compareTo(p2.getName());
+    }
+
+    public static int compareWithScore(Product p1, Product p2){
+        return p1.getAverageScore() > p2.getAverageScore() ? 1 : -1;
+    }
+
+    public static String sortOption(){
+        return "Name \n Price \n Score";
     }
 }
