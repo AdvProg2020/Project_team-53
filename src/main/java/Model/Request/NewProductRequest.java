00d68dd9944c6a6028317a14cedaec17ea7d1b73
Informatic <msaadat1379@gmail.com>
2020-05-16 16:52:05 +0430

    Edit construct of Product

--- src/main/java/Model/Request/NewProductRequest.java
+++ src/main/java/Model/Request/NewProductRequest.java
@@ -27,7 +27,9 @@ public class NewProductRequest extends Request {
 
     @Override
     public String acceptRequest() {
-        Database.addAllProduct(new Product(status, name, sellerUsername, available, number, description , categoryName, price));
+        Product product = new Product(status, name, sellerUsername, available, number, description , categoryName, price);
+        Database.addAllProduct(product);
+        Database.getCategoryByName(product.getCategoryName()).addProduct(product.getProductId());
         return "product added successfully.";
     }
 
