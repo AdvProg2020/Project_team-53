7b1a92f931f05989ef044175b32a4cb7ac8d086a
Parham <parhampch@gmail.com>
2020-05-17 19:20:22 +0430

    edit tests

--- src/test/java/ViewTest/MenuTest.java
+++ src/test/java/ViewTest/MenuTest.java
@@ -20,17 +20,4 @@ public class MenuTest {
 
         Assert.assertEquals(expected.replaceAll("\n", "").replaceAll("\r", ""), outContent.toString().replaceAll("\n", "").replaceAll("\r", ""));
     }
-
-   /* @Test
-    public void loginMenuShowTest()
-    {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        LoginMenu loginMenu = new LoginMenu(new MainMenu());
-        String expected = "Login Menu:" + "\n" + "1) Register Menu" + "\n" + "2) Login Menu" + "\n" + "3) Logout Menu" + "\n" + "4) back" + "\n";
-        loginMenu.show();
-
-        Assert.assertEquals(expected.replaceAll("\n", "").replaceAll("\r", ""), outContent.toString().replaceAll("\n", "").replaceAll("\r", ""));
-    }*/
-
 }
