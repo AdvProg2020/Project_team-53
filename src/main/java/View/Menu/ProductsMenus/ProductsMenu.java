f90ad947d89922513f690c721c67b43f89ac978d
Sony <faraz.ghahremani@yahoo.com>
2020-06-23 17:54:57 +0430

    make basic ProductsMenu

--- src/main/java/View/Menu/ProductsMenus/ProductsMenu.java
+++ src/main/java/View/Menu/ProductsMenus/ProductsMenu.java
@@ -8,8 +8,11 @@ import View.Menu.LoginMenu;
 import View.Menu.Menu;
 import View.Menu.ProductMenus.ProductMenu;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
@@ -26,32 +29,26 @@ public class ProductsMenu extends Menu {
         super.addToSubMenus(6, new LoginMenu(this));
     }
 
-    private Menu getViewAllCategoriesMenu()
-    {
+    private Menu getViewAllCategoriesMenu() {
         return new Menu("View All Categories Menu", this) {
             @Override
-            public void show()
-            {
+            public void show() {
                 System.out.println("All categories are:\n(Enter back to return)");
             }
+
             @Override
             public void execute() {
                 System.out.println(AllProductManager.showAllCategories());
                 String input = scanner.nextLine();
-                try
-                {
+                try {
                     Matcher matcher2 = getMatcher(input, "^\\s*back\\s*$");
-                    if(matcher2.find())
-                    {
+                    if (matcher2.find()) {
                         this.parentMenu.show();
                         this.parentMenu.execute();
                         return;
-                    }
-                    else
+                    } else
                         throw new Exception("Invalid Input");
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
                 this.execute();
@@ -59,8 +56,7 @@ public class ProductsMenu extends Menu {
         };
     }
 
-    private Menu getShowProductsMenu()
-    {
+    public Menu getShowProductsMenu() {
         return new Menu("Show Products Menu", this) {
             @Override
             public void show() {
@@ -69,11 +65,18 @@ public class ProductsMenu extends Menu {
                 ScrollPane scrollPane = new ScrollPane();
                 scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
                 ArrayList<Product> allProducts = Database.getAllProducts();
+                GridPane gridPane = new GridPane();
+                int row = 0;
+                int column =0;
                 for (Product product : allProducts) {
-                    mainPane.getChildren().add(getProductPane(product));
+                    //mainPane.getChildren().add(getProductPane(product));
+                    gridPane.add(getProductPane(product), column%3, row/3);
+                    column++;
+                    row++;
                 }
+                scrollPane.setContent(gridPane);
+                mainPane.setCenter(scrollPane);
                 Scene scene = new Scene(mainPane, 1000, 600);
-
                 window.setScene(scene);
                 window.show();
             }
@@ -82,36 +85,33 @@ public class ProductsMenu extends Menu {
             public void execute() {
                 System.out.println(AllProductManager.showAllProduct());
                 String input = scanner.nextLine();
-                try
-                {
+                try {
                     Matcher matcher2 = getMatcher(input, "^\\s*back\\s*$");
-                    if(matcher2.find())
-                    {
+                    if (matcher2.find()) {
                         this.parentMenu.show();
                         this.parentMenu.execute();
                         return;
-                    }
-                    else
+                    } else
                         throw new Exception("Invalid Input");
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
                 this.execute();
             }
 
-            private Pane getProductPane(Product product){
+            private Pane getProductPane(Product product) {
                 Pane pane = new Pane();
+                //Image image = new Image("./src/resource/ProductImages/notFound.png");
+                ImageView imageView = new ImageView();
+                Button button = new Button("show");
                 Label label = new Label(product.getName());
-                pane.getChildren().add(label);
+                pane.getChildren().addAll(imageView, label, button);
                 return pane;
             }
         };
     }
 
-    private Menu getProductMenu()
-    {
+    private Menu getProductMenu() {
         return new Menu("Product Menu", this) {
             @Override
             public void show() {
@@ -121,31 +121,23 @@ public class ProductsMenu extends Menu {
             @Override
             public void execute() {
                 String input = scanner.nextLine();
-                try
-                {
+                try {
                     Matcher matcher1 = getMatcher(input, "\\s*(\\d+)\\s*");
                     Matcher matcher2 = getMatcher(input, "^\\s*back\\s*$");
-                    if(matcher2.find())
-                    {
+                    if (matcher2.find()) {
                         this.parentMenu.show();
                         this.parentMenu.execute();
                         return;
-                    }
-                    else if(!matcher1.find())
-                    {
+                    } else if (!matcher1.find()) {
                         throw new Exception("invalid input");
-                    }
-                    else
-                    {
+                    } else {
                         ProductManager.setProduct(Database.getProductByID(Integer.parseInt(matcher1.group(1))));
                         ProductMenu productMenu = new ProductMenu(this.parentMenu);
                         productMenu.show();
                         productMenu.execute();
                         return;
                     }
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
                 this.execute();
