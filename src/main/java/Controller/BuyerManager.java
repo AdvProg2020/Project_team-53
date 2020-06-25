bd03a1977046c71e303bb7ec237b6849fbd50ba4
Sony <faraz.ghahremani@yahoo.com>
2020-06-25 20:05:19 +0430

    bug in pay in buyer manager fixed

--- src/main/java/Controller/BuyerManager.java
+++ src/main/java/Controller/BuyerManager.java
@@ -9,22 +9,22 @@ import Model.Product.Product;
 
 public class BuyerManager {
 
-    public static String showAllDiscounts(){
+    public static String showAllDiscounts() {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         return buyerAccount.showAllDiscounts();
     }
 
-    public static long showCostOfCart(){
+    public static long showCostOfCart() {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         return buyerAccount.getCart().getCost();
     }
 
-    public static boolean canIncreaseProduct(int productId){
+    public static boolean canIncreaseProduct(int productId) {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         return buyerAccount.getCart().increaseProduct(productId);
     }
 
-    public static String DecreaseProduct(int productId){
+    public static String DecreaseProduct(int productId) {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         if (!buyerAccount.getCart().decreaseProduct(productId))
             return "you haven't chosen this product yet";
@@ -32,12 +32,12 @@ public class BuyerManager {
         return "product eliminated successfully";
     }
 
-    public static void addNewProductToCart(Product product){
+    public static void addNewProductToCart(Product product) {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         buyerAccount.getCart().addToCart(product);
     }
 
-    public static String addProductToCart(Product product){
+    public static String addProductToCart(Product product) {
         if (product.getNumber() < 1)
             return "unfortunately we don't have this product now";
         product.setNumber(product.getNumber() - 1);
@@ -48,43 +48,43 @@ public class BuyerManager {
         return "product added to cart";
     }
 
-    public static boolean canBuy(int discountId){
+    public static boolean canBuy(int discountId) {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         long cost = buyerAccount.getCart().getCost();
         if (buyerAccount.canUseDiscount(discountId))
-            cost -= Math.min(cost*Database.getDiscountById(discountId).getPercent()/100, Database.getDiscountById(discountId).getMaxValue());
+            cost -= Math.min(cost * Database.getDiscountById(discountId).getPercent() / 100, Database.getDiscountById(discountId).getMaxValue());
         if (cost > buyerAccount.getCredit())
             return false;
         //Todo: complete the conditions
         return true;//just for make ok compile error
     }
 
-    public static void buy(int discountId){
+    public static void buy(int discountId) {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         long cost = buyerAccount.getCart().getCost();
         buyerAccount.setCredit((int) (buyerAccount.getCredit() - cost));
         payToSeller(discountId);
         // Todo: get the seller cost
-        if (buyerAccount.canUseDiscount(discountId)){
+        if (buyerAccount.canUseDiscount(discountId)) {
             buyerAccount.useDiscount(discountId);
-                if (discountId != -1) {
-                    Discount discount = Database.getDiscountById(discountId);
-                    // max value
-                    buyerAccount.setCredit(buyerAccount.getCredit() + Math.min((int) cost * discount.getPercent() / 100, discount.getMaxValue()));
-                    // Todo: check up the line above
-                } else {
-                    buyerAccount.setCredit((int) (buyerAccount.getCredit() + cost));
-                }
+            if (discountId != -1) {
+                Discount discount = Database.getDiscountById(discountId);
+                // max value
+                buyerAccount.setCredit(buyerAccount.getCredit() - Math.min((int) cost * discount.getPercent() / 100, discount.getMaxValue()));
+                // Todo: check up the line above
+            } else {
+                buyerAccount.setCredit((int) (buyerAccount.getCredit() - cost));
+            }
         }
         buyerAccount.setCart(new Cart());
     }
 
-    public static String showCart(){
+    public static String showCart() {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         return buyerAccount.getCart().showCart();
     }
 
-    public static String pay(int discountId){
+    public static String pay(int discountId) {
         if (Database.getDiscountById(discountId) == null && discountId != -1)
             return " your discount is not valid";
         if (!canBuy(discountId))
@@ -95,7 +95,7 @@ public class BuyerManager {
         }
     }
 
-    public static void payToSeller(int discountId){
+    public static void payToSeller(int discountId) {
         BuyerAccount buyerAccount = (BuyerAccount) AccountManager.getLoggedInAccount();
         int discountValue = 0;
         if (buyerAccount.canUseDiscount(discountId))
@@ -105,8 +105,14 @@ public class BuyerManager {
         for (Integer productId : buyerAccount.getCart().getProductsID()) {
             Product product = Database.getProductByID(productId);
             SellerAccount sellerAccount = (SellerAccount) Database.getAccountByUsername(product.getSellerUsername());
-            sellerAccount.setCredit(sellerAccount.getCredit() + product.getPrice()*buyerAccount.getCart().getMuchOfProductID(productId));
-            Log.addLog(buyerAccount.getUsername(), product.getSellerUsername(), product.getPrice(), productId,Math.min(product.getPrice()*product.getOff().getPercent()/100, product.getOff().getMaxValue()), discountValue*product.getPrice()/100);
+            sellerAccount.setCredit(sellerAccount.getCredit() + product.getPrice() * buyerAccount.getCart().getMuchOfProductID(productId));
+            int maxValue = product.getPrice();
+            int offValue = 0;
+            if (product.doesHaveOff()) {
+                maxValue = product.getOff().getMaxValue();
+                offValue = product.getPrice() * product.getOff().getPercent() / 100;
+            }
+            Log.addLog(buyerAccount.getUsername(), product.getSellerUsername(), product.getPrice(), productId, Math.min(offValue, maxValue), discountValue * product.getPrice() / 100);
         }
     }
 }
