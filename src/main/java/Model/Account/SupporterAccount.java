23e205239d67696e4d3d5819663c6945a4d79c87
Parham <parhampch@gmail.com>
2020-07-24 23:46:23 +0430

    debug supporter

--- src/main/java/Model/Account/SupporterAccount.java
+++ src/main/java/Model/Account/SupporterAccount.java
@@ -1,11 +1,7 @@
 package Model.Account;
 
-import Model.Messaging.Chat;
-
-import java.util.ArrayList;
-
 public class SupporterAccount extends Account {
-    private ArrayList<Chat> chats = new ArrayList<>();
+    //private ArrayList<Chat> chats = new ArrayList<>();
 
     public SupporterAccount(String username, String firstName, String lastName, String password, String email, String phoneNumber, int credit) throws Exception {
         super(username, firstName, lastName, password, email, phoneNumber, credit);
